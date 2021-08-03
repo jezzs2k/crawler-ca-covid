113862db3dcb790f92eb04d44214f9c339cf55f5
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-08-04 02:19:14 +0700

    crawler ca

---
+++ crawler.js
@@ -0,0 +1,87 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function extractNews(url) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        waitUntil: 'networkidle2',
+        // executablePath: '/usr/bin/google-chrome',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+
+    const data = await page.evaluate(() => {
+        const content = document.querySelector("section#content");
+
+        const covidNum = content.querySelector("section.container > div > div");
+
+        const table = content.querySelector("section.bg-xam > div > div table#sailorTable");
+
+        const row1 = covidNum.querySelector("div").textContent;
+        const row2 = covidNum.querySelector("div > div.row").textContent;
+
+        const rows = table.querySelectorAll('tr');
+
+        const tableData = Array.from(rows).map(item => {
+            const value = item.textContent.split("\n\t\t\t\t\t\t");
+            return {
+                province: value[1].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
+                totalCa: value[2].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
+                todayCa: value[3].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
+                dies: value[4].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
+            }
+        });
+
+        const dataRow1 = row1.split('\t\t\t\t').filter(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, "") !== '').map(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, ""));
+        const dataRow2 = row2.split('\t\t\t\t').filter(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, "") !== '').map(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, ""));
+
+        return {
+            tableData,
+            totalCa: {
+                vi: {
+                    title: dataRow1[0],
+                    treatment: dataRow1[1],
+                    beingTreated: dataRow1[2],
+                    die: dataRow1[3],
+                },
+                world: {
+                    title: dataRow2[0],
+                    treatment: dataRow2[1],
+                    beingTreated: dataRow2[2],
+                    die: dataRow2[3],
+                },
+            }
+        };
+    });
+
+    await browser.close();
+
+
+
+    return {
+        data,
+    };
+}
+
+const handleCrawlerNews = async () => {
+    const data = await extractNews('https://ncov.moh.gov.vn/');
+
+    fs.writeFile('infos.json', JSON.stringify(data), 'utf8', (err) => {
+        if (err) {
+            console.log(`Error writing file: ${err}`);
+        } else {
+            console.log(`File is written successfully!`);
+        }
+    });
+
+    return data;
+};
+
+module.exports = async () => {
+    return await handleCrawlerNews()
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const crawler = require('./crawler');
+const wakeUpDynp = require('./wakeUpDyno');
+
+const URL = 'https://crawler-news-ncov.herokuapp.com/';
+
+(async () => await crawler())();
+
+app.get('/', (req, res) => {
+    res.send('Hello my friend !')
+})
+
+app.get('/ca', async (req, res) => {
+    const data = await crawler();
+
+    res.jsonp({ data })
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+    wakeUpDynp(URL, 10);
+})
