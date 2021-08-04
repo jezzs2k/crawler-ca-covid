1fa24419696063cfef38754d4c5d845e96d04fd5
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-08-04 21:49:35 +0700

    update code

--- crawler.js
+++ crawler.js
@@ -27,15 +27,25 @@ async function extractNews(url) {
 
         const rows = table.querySelectorAll('tr');
 
+        const replaceBreakLine = (src) => {
+            return src.replace(/(\r\n|\n|\r\n|\t)/gm, "");
+        };
+
+        const replaceJustGetNumber = (src) => {
+            return src.replace(/[^\d]/g, "");
+        };
+
         const tableData = Array.from(rows).map(item => {
             const value = item.textContent.split("\n\t\t\t\t\t\t");
             return {
-                province: value[1].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
-                totalCa: value[2].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
-                todayCa: value[3].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
-                dies: value[4].replace(/(\r\n|\n|\r\n|\t)/gm, ""),
+                province: replaceBreakLine(value[1]),
+                totalCa: replaceBreakLine(value[2]),
+                todayCa: replaceBreakLine(value[3]),
+                dies: replaceBreakLine(value[4]),
             }
         });
+
+
 
         const dataRow1 = row1.split('\t\t\t\t').filter(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, "") !== '').map(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, ""));
         const dataRow2 = row2.split('\t\t\t\t').filter(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, "") !== '').map(item => item.replace(/(\r\n|\n|\r\n|\t)/gm, ""));
@@ -45,17 +55,17 @@ async function extractNews(url) {
             totalCa: {
                 vi: {
                     title: dataRow1[0],
-                    treatment: dataRow1[1],
-                    beingTreated: dataRow1[2],
-                    die: dataRow1[4],
-                    cured: dataRow1[3],
+                    treatment: replaceJustGetNumber(dataRow1[1]),
+                    beingTreated: replaceJustGetNumber(dataRow1[2]),
+                    die: replaceJustGetNumber(dataRow1[4]),
+                    cured: replaceJustGetNumber(dataRow1[3]),
                 },
                 world: {
                     title: dataRow2[0],
-                    treatment: dataRow2[1],
-                    beingTreated: dataRow2[2],
-                    die: dataRow2[4],
-                    cured: dataRow1[3],
+                    treatment: replaceJustGetNumber(dataRow2[1]),
+                    beingTreated: replaceJustGetNumber(dataRow2[2]),
+                    die: replaceJustGetNumber(dataRow2[4]),
+                    cured: replaceJustGetNumber(dataRow1[3]),
                 },
             }
         };
