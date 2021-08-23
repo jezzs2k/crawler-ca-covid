edf1c3908410027e42170df2b7a82597dd495c05
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-08-24 01:07:37 +0700

    clean code

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
 const crawler = require('./crawler');
-// const wakeUpDynp = require('./wakeUpDyno');
-
-// const URL = 'https://crawler-ca-number.herokuapp.com/';
 
 (async () => await crawler())();
 
@@ -22,5 +18,4 @@ app.get('/ca', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
-    // wakeUpDynp(URL, 25);
 })
